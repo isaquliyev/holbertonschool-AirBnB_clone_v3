248056aac3269ac9b1f4a9aea1add20a132a2f87
isaquliyev <isa.quluyev.wip@gmail.com>
2024-04-25 01:37:50 +0400

    Test environment created without environmental variables.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,10 +27,10 @@ class DBStorage:
 
     def __init__(self):
         """Instantiate a DBStorage object"""
-        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
-        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
-        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER', 'hbnb_test')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD', 'hbnb_test_pwd')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST', 'localhost')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB', 'hbnb_test_db')
         HBNB_ENV = getenv('HBNB_ENV')
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
                                       format(HBNB_MYSQL_USER,
