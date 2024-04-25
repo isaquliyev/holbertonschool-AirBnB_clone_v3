dbd92a0a5e72a81670ee6e1f701ce558b2a57304
isaquliyev <isa.quluyev.wip@gmail.com>
2024-04-25 17:12:13 +0400

    Simple json response for app_views

---
+++ api/v1/views/index.py
@@ -0,0 +1,8 @@
+from api.v1.views import app_views
+import json
+
+
+@app_views.route('/status', strict_slashes=False)
+def status():
+    """Simple function that returns status: ok"""
+    return json.dumps({"status": "OK"})
