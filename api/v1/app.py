6fabf514bae9cddac65c278f7f2f17851a222197
isaquliyev <isa.quluyev.wip@gmail.com>
2024-04-27 13:27:57 +0400

    CORS implemented

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,6 +3,7 @@
 
 
 from flask import Flask, jsonify
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -10,6 +11,7 @@ from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(404)
